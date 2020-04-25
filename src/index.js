457e8ef3d47fdb917a7fe4b8ed284a6b90c17ce2
shitman0930 <63058942+shitman0930@users.noreply.github.com>
2020-04-25 17:03:35 +0800

    uploading project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';//有了react import from新語法就能使用了
+import ReactDOM from 'react-dom';//react可以用在許多方面(手機app也可以用 VR也行) 這邊是用在建網站 所以是dom(document)
+import './index.css';//也能直接加css
+import Hello from './Hello';//開頭一定要大寫 ,要從哪個檔案進來
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';//去json確認有之後 在這邊加上 接著去Hello.js
+
+
+ReactDOM.render(<Hello greeting={'Hello ' + 'React Ninja'}/>, document.getElementById('root'));
+//greeting={} 這段也像html attribute(properties) 寫了會有錯 要去hello.js用this修 
+// {}裡面是值 參數就是Hello.js裡的props
+// props.greeting props.dog(如果這邊有Hello dog={},也能用)
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
